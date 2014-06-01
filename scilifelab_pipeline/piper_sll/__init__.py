2a55ac02ded57fbf18b0c7bf06014961befed4cf
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-06-01 16:23:46 +0200

    Sketch future plans; remove cruft; save/rename old report.tsv files

--- scilifelab_pipeline/piper_sll/__init__.py
+++ scilifelab_pipeline/piper_sll/__init__.py
@@ -11,10 +11,16 @@ from __future__ import print_function
 import collections
 import os
 import re
+import shutil
+import time
 
+from scilifelab.log import minimal_logger
 from scilifelab.utils.config import load_yaml_config_expand_vars
-from scilifelab_pipeline.common import parse_project_sample_lane_from_filename, \
-                                       find_fastq_read_pairs, get_flowcell_id_from_dirtree
+from scilifelab_pipeline.common import parse_lane_from_filename, \
+                                       find_fastq_read_pairs, \
+                                       get_flowcell_id_from_dirtree
+
+LOG = minimal_logger(__name__)
 
 def main(projects_to_analyze, config_file_path):
     """The main method.
@@ -24,6 +30,11 @@ def main(projects_to_analyze, config_file_path):
     """
     config = load_yaml_config_expand_vars(config_file_path)
     create_report_tsv(projects_to_analyze)
+
+    # Fetch requisite info for automatic config builder
+    # Run Johan's converter script if needed
+    # sbatch relevant workflow
+    # Decide how to track jobs that are running -- write to database?
 
 
 def create_report_tsv(projects_to_analyze):
@@ -40,44 +51,32 @@ def create_report_tsv(projects_to_analyze):
     :returns: The path to the report.tsv file
     :rtype: str
     """
-    report_header = "#SampleName", "Lane", "ReadLibrary", "FlowcellID"
+    report_header = ("#SampleName", "Lane", "ReadLibrary", "FlowcellID")
 
     for project in projects_to_analyze:
         report_tsv_path = os.path.join(project.base_path, project.name, "report.tsv")
         report_xml_path = os.path.join(project.base_path, project.name, "report.xml")
         if os.path.exists(report_xml_path): continue
 
-        with open("report_tsv_path", 'w') as rtsv_fh:
-            print(report_header, file=rtsv_fh)
-            for sample in project.samples:
-                for fcid in sample.fcids:
+        ## TODO Activate this check/move thing later
+        #if os.path.exists(report_tsv_path):
+        #    path, orig_filename = os.path.split(report_tsv_path)
+        #    orig_basename, orig_ext = os.path.splitext(orig_filename)
+        #    mv_filename = orig_basename + time.strftime("_%Y-%m-%d_%H:%M:%S") + orig_ext
+        #    mv_path = os.path.join(path, mv_filename)
+        #    LOG.info("Moving preexisting report.tsv file to {}".format(mv_path))
+        #    shutil.move(report_tsv_path, mv_path)
+        with open(report_tsv_path, 'w') as rtsv_fh:
+            LOG.info("Writing {}".format(report_tsv_path))
+            print("\t".join(report_header), file=rtsv_fh)
+            for sample in project:
+                for fcid in sample:
                     # How to determine lanes, readlibraries?
                     fcid_path = os.path.join(project.base_path,
                                              project.dirname,
                                              sample.dirname,
                                              fcid.dirname)
-                    for fq_pairname, fq_pair in find_fastq_read_pairs(directory=fcid_path):
-                        lane = lane_from_filename(fq_pairname)
+                    for fq_pairname in find_fastq_read_pairs(directory=fcid_path).keys():
+                        lane = parse_lane_from_filename(fq_pairname)
                         read_library = "Get this from the database somehow"
-                        print("\t".join([sample.name, lane, read_ilbrary, fcid.name]), file=rtsv_fh)
-
-    #for flowcell_dir in flowcell_dirs_to_analyze:
-    #    flowcell = get_flowcell_id_from_dirtree(flowcell_dir)
-    #    # Get file pairs
-    #    import ipdb; ipdb.set_trace()
-    #    file_dict = find_fastq_read_pairs(directory=flowcell_dir)
-    #    for fq_file in file_dict.keys():
-    #        try:
-    #            import ipdb; ipdb.set_trace()
-    #            project, sample, lane = parse_project_sample_lane_from_filename(fq_file)
-    #            ## TODO Need to figure out how to find the ReadLibrary -- I guess this is probably
-    #            ##      The "ReadLibrary" is the sequencing library that was sequenced (as you may know more than one library can be prepared from a sample)
-    #            ##      this allows us to track the sample back to the libraries sequenced and include it in the read group information in the bam files.
-    #            samples.append("\t".join(["_".join(project, sample), lane, "Check Back Later", flowcell]))
-    #        except ValueError as e:
-    #            LOG.warn(e)
-    #project_root_directory = os.pardir(os.pardir(flowcell_dir))
-    #file_location = os.path.join(project_root_directory, "report.tsv")
-    #with open(file_location, 'a+') as f:
-    #    ## TODO Need to add newlines
-    #    f.writelines(samples)
+                        print("\t".join([sample.name, lane, read_library, fcid.name]), file=rtsv_fh)
