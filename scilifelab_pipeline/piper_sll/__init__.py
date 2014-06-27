72d7a41022826b302ce8d2f3d8f67c20a5292463
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-06-27 20:10:26 +0200

    Remove fixed TODO

--- scilifelab_pipeline/piper_sll/__init__.py
+++ scilifelab_pipeline/piper_sll/__init__.py
@@ -71,7 +71,6 @@ def build_piper_cl(projects_to_analyze, config):
     # Default is the file globalConfig.xml in the piper root dir
     piper_globalconfig_path = config.get("piper", {}).get("path_to_piper_globalconfig") \
                                  or os.path.join(path_to_piper_rootdir, "globalConfig.xml")
-    ## Change this to read XML configs (after changing config to XML)
     piper_globalconfig = load_xml_config(piper_globalconfig_path)
 
     for project in projects_to_analyze:
