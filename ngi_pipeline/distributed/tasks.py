967ea5301ffa9dd4c7661e4a83742b1c57d0f0ba
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-07-03 23:07:28 +0200

    Add testing illumina_finished_msg.py. Fixes #4

--- ngi_pipeline/distributed/tasks.py
+++ ngi_pipeline/distributed/tasks.py
@@ -5,7 +5,7 @@ from celery.task import task
 
 from ngi_pipeline import common
 
-@task(ignore_results=True, quque="ngi_pipeline")
+@task(ignore_results=True, queue="ngi_pipeline")
 def launch_main_analysis(run_dir):
     """ Will call the main method in ngi_pipeline.common module.
 
