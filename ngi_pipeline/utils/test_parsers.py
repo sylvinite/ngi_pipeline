f7bfba6e4f139cdbde1b60e4d8610ba1c94b6d30
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-07-10 12:17:29 +0200

    Add tests for utils.parsers.find_fastq_read_pairs and find_fastq_read_pairs_from_dir

--- ngi_pipeline/utils/test_parsers.py
+++ ngi_pipeline/utils/test_parsers.py
@@ -1,9 +1,11 @@
 import datetime
+import os
 import random
 import tempfile
 import unittest
 
-from .parsers import get_flowcell_id_from_dirtree, parse_lane_from_filename
+from .parsers import get_flowcell_id_from_dirtree, parse_lane_from_filename, \
+                     find_fastq_read_pairs, find_fastq_read_pairs_from_dir
 from ..tests import generate_test_data as gtd
 
 class TestCommon(unittest.TestCase):
@@ -28,3 +30,21 @@ class TestCommon(unittest.TestCase):
         lane = random.randint(1,8)
         file_name = gtd.generate_sample_file_name(lane=lane)
         self.assertEqual(lane, parse_lane_from_filename(file_name))
+
+    def test_find_fastq_read_pairs(self):
+        # Test list functionality
+        file_list = [ "P123_456_AAAAAA_L001_R1_001.fastq.gz",
+                      "P123_456_AAAAAA_L001_R2_001.fastq.gz",]
+        expected_output = {"P123_456_AAAAAA_L001": file_list }
+        self.assertEqual(expected_output, find_fastq_read_pairs(file_list))
+
+    def test_find_fastq_read_pairs_from_dir(self):
+        tmp_dir = tempfile.mkdtemp()
+        file_list = map(lambda x: os.path.join(tmp_dir, x),
+                    [ "P123_456_AAAAAA_L001_R1_001.fastq.gz",
+                      "P123_456_AAAAAA_L001_R2_001.fastq.gz",])
+        for file_name in file_list:
+            # Touch the file
+            open(os.path.join(tmp_dir, file_name), 'w').close()
+        expected_output = {"P123_456_AAAAAA_L001": file_list }
+        self.assertEqual(expected_output, find_fastq_read_pairs_from_dir(tmp_dir))
