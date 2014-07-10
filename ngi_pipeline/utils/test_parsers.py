a667ec0a38bea8fc578a0fdbf67757c5419b7513
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-07-10 10:27:34 +0200

    Add test_parse_lane_from_filename

--- ngi_pipeline/utils/test_parsers.py
+++ ngi_pipeline/utils/test_parsers.py
@@ -1,8 +1,9 @@
 import datetime
+import random
 import tempfile
 import unittest
 
-from .parsers import get_flowcell_id_from_dirtree
+from .parsers import get_flowcell_id_from_dirtree, parse_lane_from_filename
 from ..tests import generate_test_data as gtd
 
 class TestCommon(unittest.TestCase):
@@ -22,3 +23,8 @@ class TestCommon(unittest.TestCase):
         self.assertEqual(get_flowcell_id_from_dirtree(sthlm_project_path), flowcell_id)
         # Test Uppsala format
         self.assertEqual(get_flowcell_id_from_dirtree(uppsala_project_path), flowcell_id)
+
+    def test_parse_lane_from_filename(self):
+        lane = random.randint(1,8)
+        file_name = gtd.generate_sample_file_name(lane=lane)
+        self.assertEqual(lane, parse_lane_from_filename(file_name))
