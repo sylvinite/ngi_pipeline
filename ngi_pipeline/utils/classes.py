f983d9f461909be8e4c861e2de91c621988e56c7
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-08-11 12:15:31 +0200

    Added with_config decorator class

--- ngi_pipeline/utils/classes.py
+++ ngi_pipeline/utils/classes.py
@@ -1,6 +1,24 @@
 import collections
 import functools
 
+from ngi_pipeline.utils.config import locate_ngi_config
+
+class with_ngi_config(object):
+    """
+    Try to load the configuration file from a list of default locations.
+    """
+    def __init__(self, f):
+        self.f = f
+        # The idea is that ths will allow wrapped functions to keep their
+        # original names, but it doesn't seem to be working as I expect
+        functools.update_wrapper(self, f)
+
+    def __call__(self, *args, **kwargs):
+        if "config_file_path" not in kwargs:
+            kwargs["config_file_path"] = locate_ngi_config()
+        return self.f(*args, **kwargs)
+
+
 class memoized(object):
     """
     Decorator, caches results of function calls.
@@ -8,6 +26,7 @@ class memoized(object):
     def __init__(self, func):
         self.func   = func
         self.cached = {}
+        functools.update_wrapper(self, func)
     def __call__(self, *args):
         if not isinstance(args, collections.Hashable):
             return self.func(*args)
