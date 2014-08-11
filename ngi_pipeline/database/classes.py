22676633f2f63e0d1789db9e9389e3ef5b8b4b3b
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-08-11 11:09:39 +0200

    Fix Charon access bug in classes.py, add the all var to __init__.py for exporting with *

--- ngi_pipeline/database/classes.py
+++ ngi_pipeline/database/classes.py
@@ -7,6 +7,7 @@ class CharonSession(requests.Session):
     def __init__(self, api_token, base_url):
         super(CharonSession, self).__init__()
         self.get = partial(self.get, headers=api_token)
-        self.post = partial(self.get, headers=api_token)
+        self.post = partial(self.post, headers=api_token)
         self.put = partial(self.put, headers=api_token)
+        self.delete = partial(self.delete, headers=api_token)
         self.base_url = base_url
