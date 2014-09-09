a155c8b6e1ccedaf51eba28889b9bf470b6fd07a
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-09-09 17:55:52 +0200

    add utils.py!

---
+++ ngi_pipeline/piper_ngi/utils.py
@@ -0,0 +1,27 @@
+import os
+
+def create_log_file_path(project, sample=None, libprep=None, seqrun=None, workflow_name=None):
+    base_path = os.path.join(project.base_path, "ANALYSIS", project.name, "logs")
+    file_name = project.name
+    if sample:
+        file_name += "-{}".format(sample.name)
+        if libprep:
+            file_name += "-{}".format(libprep.name)
+            if seqrun:
+                file_name += "-{}".format(seqrun.name)
+    if workflow_name:
+        file_name += "-{}".format(workflow_name)
+    return os.path.join(base_path, file_name) + ".log"
+
+def create_exit_code_file_path(project, sample=None, libprep=None, seqrun=None, workflow_name=None):
+    base_path = os.path.join(project.base_path, "ANALYSIS", project.name, "logs")
+    file_name = project.name
+    if sample:
+        file_name += "-{}".format(sample.name)
+        if libprep:
+            file_name += "-{}".format(libprep.name)
+            if seqrun:
+                file_name += "-{}".format(seqrun.name)
+    if workflow_name:
+        file_name += "-{}".format(workflow_name)
+    return os.path.join(base_path, file_name) + ".exit"
