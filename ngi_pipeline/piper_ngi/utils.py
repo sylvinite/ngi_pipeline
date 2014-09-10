9e5508bb399b95a43d2c478b0e3ca2b1be63383b
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-09-10 13:14:35 +0200

    Synchronize local process exit codes with Charon; bugfixes

--- ngi_pipeline/piper_ngi/utils.py
+++ ngi_pipeline/piper_ngi/utils.py
@@ -1,25 +1,36 @@
 import os
 
-def create_log_file_path(workflow_name, project, sample=None, libprep=None, seqrun=None):
-    base_path = os.path.join(project.base_path, "ANALYSIS", project.name, "logs")
-    file_name = project.name
-    if sample:
-        file_name += "-{}".format(sample.name)
-        if libprep:
-            file_name += "-{}".format(libprep.name)
-            if seqrun:
-                file_name += "-{}".format(seqrun.name)
-    file_name += "-{}".format(workflow_name)
-    return os.path.join(base_path, file_name) + ".log"
+def create_log_file_path(workflow_name, project_base_path, project_name,
+                         sample_id=None, libprep_id=None, seqrun_id=None):
+    file_base_pathname = _create_generic_output_file_path(workflow_name,
+                                                          project_base_path,
+                                                          project_name,
+                                                          sample_id,
+                                                          libprep_id,
+                                                          seqrun_id)
+    return file_base_pathname + ".log"
 
-def create_exit_code_file_path(workflow_name, project, sample=None, libprep=None, seqrun=None):
-    base_path = os.path.join(project.base_path, "ANALYSIS", project.name, "logs")
-    file_name = project.name
-    if sample:
-        file_name += "-{}".format(sample.name)
-        if libprep:
-            file_name += "-{}".format(libprep.name)
-            if seqrun:
-                file_name += "-{}".format(seqrun.name)
+
+def create_exit_code_file_path(workflow_name, project_base_path, project_name,
+                               sample_id=None, libprep_id=None, seqrun_id=None):
+    file_base_pathname = _create_generic_output_file_path(workflow_name,
+                                                          project_base_path,
+                                                          project_name,
+                                                          sample_id,
+                                                          libprep_id,
+                                                          seqrun_id)
+    return file_base_pathname + ".exit"
+
+
+def _create_generic_output_file_path(workflow_name, project_base_path, project_name,
+                                     sample_id=None, libprep_id=None, seqrun_id=None):
+    base_path = os.path.join(project_base_path, "ANALYSIS", project_name, "logs")
+    file_name = project_name
+    if sample_id:
+        file_name += "-{}".format(sample_id)
+        if libprep_id:
+            file_name += "-{}".format(libprep_id)
+            if seqrun_id:
+                file_name += "-{}".format(seqrun_id)
     file_name += "-{}".format(workflow_name)
-    return os.path.join(base_path, file_name) + ".exit"
+    return os.path.join(base_path, file_name)
