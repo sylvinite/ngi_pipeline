e62d32cadadd64afbcaeee8694674d7ac646e755
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-09-16 10:25:35 +0200

    Add a timeout to GET requests to Charon

--- ngi_pipeline/database/classes.py
+++ ngi_pipeline/database/classes.py
@@ -40,10 +40,10 @@ class CharonSession(requests.Session):
     #                                   headers=self._api_token_dict,
     #                                   *args, **kwargs))
 
-        self.get = validate_response(functools.partial(self.get, headers=self._api_token_dict))
-        self.post = validate_response(functools.partial(self.post, headers=self._api_token_dict))
-        self.put = validate_response(functools.partial(self.put, headers=self._api_token_dict))
-        self.delete = validate_response(functools.partial(self.delete, headers=self._api_token_dict))
+        self.get = validate_response(functools.partial(self.get, headers=self._api_token_dict, timeout=10))
+        self.post = validate_response(functools.partial(self.post, headers=self._api_token_dict, timeout=10))
+        self.put = validate_response(functools.partial(self.put, headers=self._api_token_dict, timeout=10))
+        self.delete = validate_response(functools.partial(self.delete, headers=self._api_token_dict, timeout=10))
 
         self._project_params = ("projectid", "name", "status", "pipeline", "bpa")
         self._sample_params = ("sampleid", "status", "received", "qc_status",
