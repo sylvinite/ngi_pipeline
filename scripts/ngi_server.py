331280194cc42fb7b91bf682273c09dbf92827b4
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-10-15 17:44:31 +0200

    NGI server working now

--- scripts/ngi_server.py
+++ scripts/ngi_server.py
@@ -10,27 +10,21 @@ import argparse
 
 from subprocess import check_call
 
-from ngi_pipeline.utils.config import load_yaml_config
 from ngi_pipeline.distributed.celery import app
 
 
-
-def run_server(config_file, queues=None, task_module=None):
-    """ Loads configuration and launches the server
+def run_server(task_module=None):
+    """ Launches the server
     """
-    config = load_yaml_config(config_file)
-
     cl = ["celery"]
-    tasks = task_module if task_module else "ngi_pipeline.distributed.tasks"
-    cl += ["-A", tasks, "worker", "-n", "ngi_server"]
+    tasks = task_module if task_module else "ngi_pipeline.distributed.celery.tasks"
+    cl += ["-A", tasks, "worker", "-n", "ngi_server", "--loglevel=info"]
     check_call(cl)
 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", required=True,
-            help="The path to the configuration file.")
     parser.add_argument("-t", "--tasks", dest="task_module", action="store",
                       default=None, help="Task module to import")
     args = parser.parse_args()
-    run_server(args.config, args.task_module)
+    run_server(args.task_module)
