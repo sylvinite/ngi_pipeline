a98a374a7693579a4c45aebae35f21a46ce474b2
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-10-15 13:20:29 +0200

    Add create_sbatch_file function

--- ngi_pipeline/engines/piper_ngi/utils.py
+++ ngi_pipeline/engines/piper_ngi/utils.py
@@ -1,5 +1,39 @@
 import os
 
+
+SBATCH_HEADER = """
+#!/bin/bash -l
+
+#SBATCH -A {project_id}
+#SBATCH -p {slurm_queue}
+#SBATCH -n {num_cores}
+#SBATCH -t {slurm_time}
+#SBATCH -J {job_name}
+#SBATCH -o {slurm_out_log}
+#SBATCH -e {slurm_err_log}
+
+"""
+
+def create_sbatch_file(project_id, slurm_queue, num_cores, slurm_time,
+                       job_name, slurm_out_log, slurm_err_log,
+                       command_line_list, path_to_sbatch_file):
+    """
+    Note that the path to the sbatch file must be the full path including
+    the desired filename.
+    """
+    sbatch_text = SBATCH_HEADER.format(project_id=project_id,
+                                       slurm_queue=slurm_queue,
+                                       num_cores=num_cores,
+                                       slurm_time=slurm_time,
+                                       job_name=job_name,
+                                       slurm_out_log=slurm_out_log,
+                                       slurm_err_log=slurm_err_log)
+    for command_line in command_line_list:
+        sbatch_text += "\n{}".format(command_line)
+    with open(path_to_sbatch_file, 'w') as f:
+        f.write(sbatch_text)
+        f.write("\n")
+
 def create_log_file_path(workflow_subtask, project_base_path, project_name,
                          sample_id=None, libprep_id=None, seqrun_id=None):
     file_base_pathname = _create_generic_output_file_path(workflow_subtask,
