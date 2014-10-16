1ff7ab2df701787e4638ee43406e9d6e3affe822
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-10-16 13:24:13 +0200

    NGI server, Tornado version

---
+++ scripts/ngi_server_tornado.py
@@ -0,0 +1,24 @@
+""" Small Tornado server to trigger bioinforamatics analysis.
+"""
+import tornado.ioloop
+import tornado.web
+
+from ngi_pipeline.conductor import flowcell
+
+class ProcessDemultiplexedFlowcellHandler(tornado.web.RequestHandler):
+    def get(self, flowcell_id):
+        restrict_to_projects = self.get_argument('restrict_to_projects', None)
+        restrict_to_samples = self.get_argument('restrict_to_samples', None)
+        restart_failed_jobs = self.get_argument('restart_failed_jobs', False)
+        config_file_path = self.get_argument('config_file_path', None)
+        self.write("Flowcell {} started being processed".format(flowcell_id))
+
+application = tornado.web.Application([
+    # Here a regex that really matches a flowcell
+    (r"/ngi_pipeline/process_demultiplexed_flowcell/([^/]*)$", ProcessDemultiplexedFlowcellHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+
