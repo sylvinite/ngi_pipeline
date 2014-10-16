d6837ac80e7ab88556a474386fe36098ca8db3a8
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-10-16 16:09:18 +0200

    Remove celery stuff, saved on a branch in my repo

--- ngi_pipeline/distributed/celery/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Celery module for NGI-pipeline
-"""
-from celery import Celery
-
-app = Celery('ngi_celery_server',
-             broker='redis://localhost:6379/0',
-             backend='redis://localhost:6379/0')
