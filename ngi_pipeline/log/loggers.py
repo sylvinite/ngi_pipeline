4b34b38a2db766fa42a70dfd5b54415b0d0ac065
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-10-31 09:48:26 +0100

    Merge minimal_logger with file logger

--- ngi_pipeline/log/loggers.py
+++ ngi_pipeline/log/loggers.py
@@ -2,6 +2,7 @@
 log module
 """
 import logging
+import os
 import sys
 
 #from ngi_pipeline.utils.config import load_yaml_config
@@ -32,10 +33,11 @@ def _enqueue_output(output_buffer, queue, logging_fn):
     output_buffer.close()
 
 
-def minimal_logger(namespace, debug=False):
-    """Make and return a minimal console logger.
+def minimal_logger(namespace, config_file=None, to_file=True, debug=False):
+    """Make and return a minimal console logger. Optionally write to a file as well.
 
     :param namespace: String - namespace of logger
+    :param to_file: Boolean - Log to a file (location in configuration file)
     :param debug: Boolean - Log in DEBUG level or not
 
     :returns: A logging.Logger object
@@ -44,41 +46,29 @@ def minimal_logger(namespace, debug=False):
     log_level = logging.DEBUG if debug else logging.INFO
     log = logging.getLogger(namespace)
     log.setLevel(log_level)
+
+    # Console logger
     s_h = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     s_h.setFormatter(formatter)
     s_h.setLevel(log_level)
     log.addHandler(s_h)
 
+    # File logger
+    if to_file:
+        cwd = os.path.dirname(os.path.realpath('.'))
+        if not config_file:
+            log.warn("No configuration file specified, logging in {}/ngi_pipeline.log".format(cwd))
+        else:
+            config = cl.load_config(config_file)
+            if not config.get('log_dir'):
+                log.warn("No logging path specified, logging in {}/ngi_pipeline.log".format(cwd))
+            else:
+                log_path = os.path.join(config.get('log_dir'), 'ngi_pipeline.log')
+                log.info("Logging in file {}/ngi_pipeline.log".format(log_path))
+                fh = logging.FileHandler(log_path)
+                fh.setLevel(log_level)
+                fh.setFormatter(formatter)
+                log.addHandler(fh)
+
     return log
-
-
-# Uh yeah I don't think this works
-def file_logger(namespace, config_file , log_file, log_path_key = None):
-    CONFIG = cl.load_config(config_file)
-    if not log_path_key:
-        log_path = CONFIG['log_dir'] + '/' + log_file
-    else:
-        log_path = CONFIG[log_path_key] + '/' + log_file
-
-    logger = logging.getLogger(namespace)
-    logger.setLevel(logging.DEBUG)
-
-    # file handler:
-    fh = logging.FileHandler(log_path)
-    fh.setLevel(logging.INFO)
-
-    # console handler
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-
-    # formatter
-    formatter = logging.Formatter("%(asctime)s (%(levelname)s) : %(message)s")
-    fh.setFormatter(formatter)
-
-    # add handlers to logger
-    logger.addHandler(ch)
-    logger.addHandler(fh)
-
-    return logger
-
