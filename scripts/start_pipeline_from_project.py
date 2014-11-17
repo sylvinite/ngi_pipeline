6c4e8576ecfd766b1affa96d50b8a145ae8c5e2a
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-11-17 09:56:13 +0100

    Bugfix

--- scripts/start_pipeline_from_project.py
+++ scripts/start_pipeline_from_project.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 
     args_dict = vars(parser.parse_args())
 
-    project = recreate_project_from_filesystem(args_dict['project_dir'],
+    project = recreate_project_from_filesystem(args_dict['project_dir'].pop(),
                                                args_dict['restrict_to_samples'])
     if project and os.path.split(project.base_path)[1] == "DATA":
         project.base_path = os.path.split(project.base_path)[0]
