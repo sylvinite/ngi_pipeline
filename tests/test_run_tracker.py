788e0c9d296429cb515f29b49722a842a99a3e14
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-11-24 17:17:58 +0100

    Create temporal directory structure

--- tests/test_run_tracker.py
+++ tests/test_run_tracker.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+import shutil
 import unittest
 
 from run_tracker import *
@@ -7,13 +9,35 @@ from run_tracker import *
 class TestTracker(unittest.TestCase):
     """ run_tracker.py script tests
     """
-    def __init__(self):
-        pass
-
     @classmethod
     def setUpClass(self):
-        print 'yuhhh'
+        """ Creates the following directory tree for testing purposes:
+
+        tmp/
+         ├── 141124_FINISHED
+         │   └── Demultiplexing
+         │       └── Stats
+         │           └── DemultiplexingStats.xml
+         ├── 141124_IN_PROGRESS_FCIDXX
+         │   └── Demultiplexing
+         └── 141124_TOSTART_FCIDXX
+        """
+        self.tmp_dir = 'tmp'
+        self.to_start = os.path.join(self.tmp_dir, '141124_TOSTART_FCIDXX')
+        self.in_progress = os.path.join(self.tmp_dir, '141124_IN_PROGRESS_FCIDXX')
+        self.finished = os.path.join(self.tmp_dir, '141124_FINISHED')
+        os.makedirs(self.tmp_dir)
+        os.makedirs(self.to_start)
+        os.makedirs(os.path.join(self.in_progress, 'Demultiplexing'))
+        os.makedirs(os.path.join(self.finished, 'Demultiplexing', 'Stats'))
+        open(os.path.join(self.finished, 'Demultiplexing', 'Stats', 'DemultiplexingStats.xml'), 'w').close()
 
     @classmethod
     def tearDownClass(self):
-        print 'oh...'
+        shutil.rmtree(self.tmp_dir)
+
+
+    def test_1_is_finished(self):
+        """ Is finished should be True only if "RTAComplete.txt" file is present...
+        """
+        self.assertEqual('a', 'a')
