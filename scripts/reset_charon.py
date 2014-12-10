569161de5ac69962a162f446e59e73d228a7a22c
Francesco Vezzi <vezzi@nestor1.uppmax.uu.se>
2014-12-10 12:12:49 +0100

    script to clean projects on charon

--- scripts/reset_charon.py
+++ scripts/reset_charon.py
@@ -5,79 +5,40 @@ import os
 import glob
 import re
 
-from ngi_pipeline.database import construct_charon_url, get_charon_session
-from ngi_pipeline.log import minimal_logger
-from ngi_pipeline.utils.config import load_yaml_config, locate_ngi_config
-from ngi_pipeline.database import get_project_id_from_name
-from ngi_pipeline.utils.parsers import parse_genome_results
+
+import argparse
+import time
+import os
+
+from ngi_pipeline.database.classes import CharonSession, CharonError
+from ngi_pipeline.log.loggers import minimal_logger
+
+
+def main(project):
+
+    charon_session = CharonSession()
+    samples = charon_session.project_get_samples(project)
+    for sample in samples["samples"]:
+        
+        charon_session.sample_update(project, sample["sampleid"],
+                                     analysis_status= "TO_ANALYZE",
+                                     genotyping_status=None,
+                                     total_autosomal_coverage="0",
+                                     total_sequenced_reads="0")
+        for sample_prep in charon_session.sample_get_libpreps(project, sample["sampleid"])['libpreps']:
+            seqruns = charon_session.libprep_get_seqruns(project, sample["sampleid"], sample_prep["libprepid"])['seqruns']
+            for seqrun in seqruns:
+                charon_session.seqrun_update(project, sample["sampleid"], sample_prep["libprepid"], seqrun["seqrunid"],
+                                             mean_autosomal_coverage = "0",
+                                             alignment_status  = "NOT_RUNNING")
+
+
 
 
 if __name__ == '__main__':
-    
-    
-    charon_session = get_charon_session()
-    url = construct_charon_url("projects")
-    
-    projects_response = charon_session.get(url).json()
-    
-    projects_to_clean = ("P1142", "P1171", "P567")
-    
-    for project in projects_response["projects"]:
-        if project["projectid"] in projects_to_clean:
-            project["pipeline"] = "NGI"
-            project["best_practice_analysis"] = "IGN"
-            url = construct_charon_url("project", project["projectid"])
-            charon_session.put(url, json.dumps(project))
-            
-            """
-            url = construct_charon_url("samples", project["projectid"])
-            samples_response = charon_session.get(url).json()
-            for sample in samples_response["samples"]:
-                if "total_autosomal_coverage" in sample:
-                    del sample["total_autosomal_coverage"]
-                sample["total_autosomal_coverage"] = 0.0
-                if "status" in sample:
-                    del sample["status"]
-                url = construct_charon_url("sample", project["projectid"], sample["sampleid"])
-                charon_session.put(url, json.dumps(sample))
-            
-            url =construct_charon_url("seqruns", project["projectid"])
-            seqruns_response = charon_session.get(url).json()
-            for seqrun in seqruns_response["seqruns"]:
-                fields_to_delete = (
-                        'mean_autosome_coverage',
-                        'mean_coverage',
-                        'std_coverage',
-                        'aligned_bases',
-                        'mapped_bases',
-                        'mapped_reads',
-                        'reads',
-                        'sequenced_bases',
-                        'bam_file',
-                        'output_file',
-                        'GC_percentage',
-                        'mean_mapping_quality',
-                        'bases_number',
-                        'contigs_number',
-                        'lanes'
-                        'alignment_status'
-                        )
-                
-                for field in fields_to_delete:
-                    if field in seqrun:
-                        del(seqrun[field])
-                
-                
-                
-                seqrun['mean_autosome_coverage'] = 0
-
-                url = construct_charon_url("seqrun", seqrun["projectid"],
-                    seqrun["sampleid"], seqrun["libprepid"], seqrun["seqrunid"])
-                charon_session.put(url, json.dumps(seqrun))
-
-
-            """
-
-
+    parser = argparse.ArgumentParser("Clean the specify project: restore it to default --- no sample will be deleted")
+    parser.add_argument("-p", "--project", dest="project", help=("Project to restore "))
+    args_dict = vars(parser.parse_args())
+    main(**args_dict)
 
 
