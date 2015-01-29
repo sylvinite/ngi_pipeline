f1f595b2b45a876fff614ccba6447b4320d38314
Denis Moreno <denis@nestor1.uppmax.uu.se>
2015-01-29 13:10:11 +0100

    removed inspector gadget

--- ngi_pipeline/conductor/launchers.py
+++ ngi_pipeline/conductor/launchers.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 
 import importlib
 import os
-import inspect
 
 from ngi_pipeline.conductor.classes import NGIProject
 from ngi_pipeline.database.classes import CharonSession, CharonError
