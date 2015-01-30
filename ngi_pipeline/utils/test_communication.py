7edce2500d26c6bf1c754751d1f1a5d3e1db1c01
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2015-01-30 15:44:26 +0100

    Get weird with the formatting

--- ngi_pipeline/utils/test_communication.py
+++ ngi_pipeline/utils/test_communication.py
@@ -1,16 +1,17 @@
-
 import unittest
-from communication import mail, mail_sample_analysis
-
-
+from communication import mail_analysis
 
 class TestCommunication(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.project_name = "Y.Mom_15_01"
         cls.sample_name = "P1155_101"
-        cls.workflow_name = "variant_calling"
-        
-    def test_mail_sample_analysis(self):
-        mail_sample_analysis(self.project_name, self.sample_name, self.workflow_name)
+        cls.engine_name = "piper_ngi"
+        cls.err_text = "Your mom goes to college."
+
+    def test_mail_analysis(self):
+        mail_analysis(project_name=self.project_name,
+                      sample_name=self.sample_name,
+                      engine_name=self.engine_name,
+                      info_text=self.err_text)
 
