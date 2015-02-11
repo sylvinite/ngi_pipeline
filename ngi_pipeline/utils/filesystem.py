befb99d3e62c8a8e8dfd8854ce8614bac8f8f00b
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2015-02-11 12:21:38 +0100

    Use octal for mode

--- ngi_pipeline/utils/filesystem.py
+++ ngi_pipeline/utils/filesystem.py
@@ -132,7 +132,7 @@ def do_rsync(src_files, dst_dir):
     return [ os.path.join(dst_dir,os.path.basename(f)) for f in src_files ]
 
 
-def safe_makedir(dname, mode=0777):
+def safe_makedir(dname, mode=0o0770):
     """Make a directory (tree) if it doesn't exist, handling concurrent race
     conditions.
     """
