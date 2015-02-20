eb2bb57864f8db3d6be3441f30c846a50552b8d8
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2015-02-20 15:51:48 +0100

    -v--

--- ngi_pipeline/engines/piper_ngi/utils.py
+++ ngi_pipeline/engines/piper_ngi/utils.py
@@ -38,8 +38,8 @@ def get_finished_seqruns_for_sample(project_id, sample_id,
                 if aln_status == "DONE":
                     libpreps[libprep_id].append(seqrun_id)
                 else:
-                    LOG.info('Skipping seqrun "{}" due to alignment_status '
-                             '"{}"'.format(seqrun_id, aln_status))
+                    LOG.debug('Skipping seqrun "{}" due to alignment_status '
+                              '"{}"'.format(seqrun_id, aln_status))
         else:
             LOG.info('Skipping libprep "{}" due to qc status '
                      '"{}"'.format(libprep, libprep.get("qc")))
