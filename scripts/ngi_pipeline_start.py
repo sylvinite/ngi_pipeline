bb0214237a4a0aa4382a361a8c6d414626fca979
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2015-03-10 11:41:02 +0100

    Add parser & options for organize flowcell

--- scripts/ngi_pipeline_start.py
+++ scripts/ngi_pipeline_start.py
@@ -19,7 +19,8 @@ LOG = minimal_logger("ngi_pipeline_start")
 
 class ArgumentParserWithTheFlagsThatIWant(argparse.ArgumentParser):
     def __init__(self, *args, **kwargs):
-        super(ArgumentParserWithTheFlagsThatIWant, self).__init__(*args, **kwargs)
+        super(ArgumentParserWithTheFlagsThatIWant, self).__init__(*args,
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter, **kwargs)
         self.add_argument("-f", "--restart-failed", dest="restart_failed_jobs", action="store_true",
                 help=("Restart jobs marked as 'FAILED' in Charon"))
         self.add_argument("-d", "--restart-done", dest="restart_finished_jobs", action="store_true",
@@ -41,28 +42,52 @@ if __name__ == "__main__":
     parser_server = subparsers.add_parser('server', help="Start ngi_pipeline server")
     parser_server.add_argument('-p', '--port', type=int, help="Port in where to run the application")
 
-    # Add subparser for organizing a flowcell
+
+    # Add subparser for organization
     parser_organize = subparsers.add_parser('organize',
-        help="Organize a flowcell into project/sample/libprep/seqrun format.")
-    subparsers_organize = parser_organize.add_subparsers()
+            help="Organize a flowcell into project/sample/libprep/seqrun format.")
+    subparsers_organize = parser_organize.add_subparsers(help='Choose unit to analyze')
+    # Add sub-subparser for flowcell organization
+    ### TODO change to work with multiple flowcells
+    organize_flowcell = subparsers_organize.add_parser('flowcell',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            help='Organize a raw flowcell, populating Charon with relevant data.')
+    organize_flowcell.add_argument("organize_fc_dir", action="store",
+            help=("The path to the Illumina demultiplexed fc directory to organize"))
+    organize_flowcell.add_argument("-l", "--sequencing-facility", default="NGI-S", choices=('NGI-S', 'NGI-U'),
+            help="The facility where sequencing was performed.")
+    organize_flowcell.add_argument("-b", "--best_practice_analysis", default="whole_genome_reseq",
+			help="The best practice analysis to run for this project.")
+    organize_flowcell.add_argument("-f", "--force", dest="force_update", action="store_true",
+			help="Force updating Charon projects. Danger danger danger. This will overwrite things.")
+    organize_flowcell.add_argument("-d", "--delete", dest="delete_existing", action="store_true",
+			help="Delete existing projects in Charon. Similarly dangerous.")
+    organize_flowcell.add_argument("--force-create-project", action="store_true",
+            help="TESTING ONLY: Create the project if it does not exist in Charon using the project name as the project id.")
+    organize_flowcell.add_argument("-s", "--sample", dest="restrict_to_samples", action="append",
+			help="Restrict processing to these samples. Use flag multiple times for multiple samples.")
+    organize_flowcell.add_argument("-p", "--project", dest="restrict_to_projects", action="append",
+			help="Restrict processing to these projects. Use flag multiple times for multiple projects.")
+
 
     # Add subparser for analysis
     parser_analyze = subparsers.add_parser('analyze', help="Launch analysis.")
     subparsers_analyze = parser_analyze.add_subparsers(parser_class=ArgumentParserWithTheFlagsThatIWant,
             help='Choose unit to analyze')
-
-    # Another subparser for flowcell analysis
+    # Add sub-subparser for flowcell analysis
+    ### TODO change to work with multiple flowcells
     analyze_flowcell = subparsers_analyze.add_parser('flowcell', help='Start analysis of raw flowcells')
-    analyze_flowcell.add_argument("demux_fcid_dir", action="store",
-            help=("The path to the Illumina demultiplexed fc directories "
+    analyze_flowcell.add_argument("analyze_fc_dir", action="store",
+            help=("The path to the Illumina demultiplexed fc directory "
                   "to process and analyze."))
     analyze_flowcell.add_argument("-p", "--project", dest="restrict_to_projects", action="append",
             help=("Restrict analysis to these projects. "
                   "Use flag multiple times for multiple projects."))
-
-    # Another subparser for project analysis
+    # Add sub-subparser for project analysis
+    ### TODO change to work with multiple projects
     project_group = subparsers_analyze.add_parser('project', help='Start the analysis of a pre-parsed project.')
-    project_group.add_argument('project_dir', action='store', help='The path to the project folder to be analyzed.')
+    project_group.add_argument('analyze_project_dir', action='store', help='The path to the project folder to be analyzed.')
+
 
     args = parser.parse_args()
 
@@ -73,16 +98,18 @@ if __name__ == "__main__":
         args.restart_running_jobs = True
 
     # Finally execute corresponding functions
-    if 'demux_fcid_dir' in args:
-        LOG.info("Starting flowcell analysis in directory {}".format(args.demux_fcid_dir))
-        flowcell.process_demultiplexed_flowcell(args.demux_fcid_dir,
+    ### TODO change to work with multiple flowcells
+    if 'analyze_fc_dir' in args:
+        LOG.info("Starting flowcell analysis in directory {}".format(args.analyze_fc_dir))
+        flowcell.process_demultiplexed_flowcell(args.analyze_fc_dir,
                                                 args.restrict_to_projects,
                                                 args.restrict_to_samples,
                                                 args.restart_failed_jobs,
                                                 args.restart_finished_jobs,
                                                 args.restart_running_jobs)
-    elif 'project_dir' in args:
-        project = recreate_project_from_filesystem(project_dir=args.project_dir,
+    ### TODO change to work with multiple projects
+    elif 'analyze_project_dir' in args:
+        project = recreate_project_from_filesystem(project_dir=args.analyze_project_dir,
                                                    restrict_to_samples=args.restrict_to_samples)
         if project and os.path.split(project.base_path)[1] == "DATA":
             project.base_path = os.path.split(project.base_path)[0]
@@ -90,6 +117,8 @@ if __name__ == "__main__":
                                   restart_failed_jobs=args.restart_failed_jobs,
                                   restart_finished_jobs=args.restart_finished_jobs,
                                   restart_running_jobs=args.restart_running_jobs)
+    elif 'organize_fc_dir' in args:
+        import ipdb; ipdb.set_trace()
     elif 'port' in args:
         LOG.info('Starting ngi_pipeline server at port {}'.format(args.port))
         server_main.start(args.port)
