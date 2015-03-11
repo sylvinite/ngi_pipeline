853a50db85a80b5cee7e2b6df85c0d68fc8e1270
Denis Moreno <denis@nestor1.uppmax.uu.se>
2015-03-11 15:26:43 +0100

    Everyone knows that None > 0

--- ngi_pipeline/engines/piper_ngi/local_process_tracking.py
+++ ngi_pipeline/engines/piper_ngi/local_process_tracking.py
@@ -66,7 +66,7 @@ def update_charon_with_local_jobs_status(config=None, config_file_path=None):
                               engine_name=engine, level="ERROR", info_text=error_text)
                 continue
             try:
-                if piper_exit_code == 0:
+                if piper_exit_code and piper_exit_code == 0:
                     # 0 -> Job finished successfully
                     set_status = "ANALYZED"
                     info_text = ('Workflow "{}" for {} finished succesfully. '
@@ -91,7 +91,7 @@ def update_charon_with_local_jobs_status(config=None, config_file_path=None):
                     piper_qc_dir = os.path.join(project_base_path, "ANALYSIS",
                                                 project_id,"piper_ngi",  "02_preliminary_alignment_qc")
                     update_coverage_for_sample_seqruns(project_id, sample_id, piper_qc_dir)
-                elif piper_exit_code >0:
+                elif piper_exit_code and piper_exit_code >0:
                     # 1 -> Job failed
                     set_status = "FAILED"
                     error_text = ('Workflow "{}" for {} failed. Recording status '
