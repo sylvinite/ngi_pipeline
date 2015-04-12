4bd0b0c90abe4936bd7ba27f9b13efed686e6326
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2015-04-12 20:59:30 +0200

    Don't die if project is missing in Charon in when launching project analysis

--- ngi_pipeline/conductor/launchers.py
+++ ngi_pipeline/conductor/launchers.py
@@ -33,7 +33,11 @@ def launch_analysis(projects_to_analyze, restart_failed_jobs=False,
         engine.local_process_tracking.update_charon_with_local_jobs_status(config=config)
     charon_session = CharonSession()
     for project in projects_to_analyze:
-        project_status = charon_session.project_get(project.project_id)['status']
+        try:
+            project_status = charon_session.project_get(project.project_id)['status']
+        except CharonError as e:
+            LOG.error('Project {} could not be processed: {}'.format(project, e))
+            continue
         if not project_status == "OPEN":
             error_text = ('Data found on filesystem for project "{}" but Charon '
                           'reports its status is not OPEN ("{}"). Not launching '
