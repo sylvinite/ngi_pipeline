b81c1d3fc4dea3a59ddbbd57c8bec77548cabce8
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2015-05-13 21:32:58 +0200

    Add update_dict function

--- ngi_pipeline/utils/pyutils.py
+++ ngi_pipeline/utils/pyutils.py
@@ -1,5 +1,6 @@
 """General Python-specific utility functions, in most cases probably skimmed off StackOverflow."""
 import collections
+import copy
 
 def flatten(nested_list):
     """All I ever need to know about flattening irregular lists of lists I learned from
@@ -10,3 +11,23 @@ def flatten(nested_list):
                 yield sub
         else:
             yield elt
+
+def update_dict(original_dict, updater_dict):
+    """Updates a dictionary in a nested fashion, replacing and adding individual
+    keys/values but not overwriting entire nested dicts.
+
+    :param dict original_dict: The dict to update
+    :param dict updater_dict: The dict with the keys/values to update the original
+
+    :return: A copy of the original dict updated with the new keys/values
+    :rtype: dict
+    """
+
+    updated_dict = copy.deepcopy(original_dict)
+    for key, value in updater_dict.iteritems():
+        if updated_dict.get(key) and type(updated_dict[key]) is dict and type(value) is dict:
+            # If the value itself is a dict, we want to update, not overwrite
+            updated_dict[key] = update_dict(updated_dict[key], value)
+        else:
+            updated_dict[key] = value
+    return updated_dict
