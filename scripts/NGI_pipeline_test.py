a7983fc166761edd437f020334cb8a2bb73fe061
Francesco Vezzi <vezzi@nestor1.uppmax.uu.se>
2016-03-21 12:08:02 +0100

    improved argparse, implemented pontus suggestions

--- scripts/NGI_pipeline_test.py
+++ scripts/NGI_pipeline_test.py
@@ -11,27 +11,43 @@ import shutil
 
 
 def main(args):
-    if args.delete is not None:
-        print "deleting project {} from charon".format(args.delete)
-        cleanCharon(args.delete,args.url, args.token)
-        print "I am leazy and I am not going to do nothing more than this"
-        return 0
-
+    if hasattr(args, 'project_id'):
+        # we are in delete mode
+        print "deleting project {} from charon".format(args.project_id)
+        cleanCharon(args.project_id,args.url, args.token)
+        print "  Done"
+    elif hasattr(args, 'project_name'):
+        # we are in create mode
+        create_project(args.project_name,
+                    args.fastq1,
+                    args.fastq2,
+                    args.facility,
+                    args.FC,
+                    args.symlinks,
+                    args.token,
+                    args.url
+                    )
+    else:
+        print "something strange going on..."
+        return 1
+    return 0
+    
+def create_project(project_name, fastq1, fastq2, facility, FC, symlinks, token, url):
     # create fake project name and fake project idenitifier
-    (project_name, project_identifier) = create_proj_name(args.project_name, args.facility)
+    (project_name, project_identifier) = create_proj_name(project_name, facility, token, url)
     # create the data structure that will contains all the project, samples, libraries, runs
     charon_data = {}
     charon_data['projectid'] = project_identifier
     charon_data['name'] = project_name
-    charon_data['pipeline']="TEST"
-    charon_data['sequencing_facility'] = "NGI-S" if args.facility == 'stockholm' else "NGI-U"
+    charon_data['sequencing_facility'] = "NGI-S" if facility == 'stockholm' else "NGI-U"
     charon_data['best_practice_analysis']="whole_genome_reseq"
     charon_data['status']='OPEN'
     charon_data['pipeline']='NGI'
     charon_data['samples']={}
     # now iterate over all FC I need to create
     sample_number = 1 # this needs to keep count of the samples
-    for i in xrange(args.FC):
+    FCs = []
+    for i in xrange(FC):
         #create fake FC name
         FC_name = create_fake_FC()
         # create directory
@@ -49,9 +65,10 @@ def main(args):
         os.mkdir(os.path.join(FC_name, "Demultiplexing", project_name_underscore))
         # for each lane create a sample (make stockholm uppsala difference)
         print "producing FC: {}".format(FC_name)
+        FCs.append(FC_name)
         for lane in (1,2,3,4,5,6,7,8):
             # create fake sample names, start easy, one sample per lane
-            sample_name = create_sample_name(project_identifier, sample_number, args.facility)
+            sample_name = create_sample_name(project_identifier, sample_number, facility)
             # create the sample folder
             sample_dir = os.path.join(FC_name, "Demultiplexing", project_name_underscore, "Sample_{}".format(sample_name))
             os.mkdir(sample_dir)
@@ -75,22 +92,24 @@ def main(args):
             # now copy the fastq file
             fastq1_dest = os.path.join(sample_dir, "{}_S{}_L00{}_R1_001.fastq.gz".format(sample_name, lane, lane))
             fastq2_dest = os.path.join(sample_dir, "{}_S{}_L00{}_R2_001.fastq.gz".format(sample_name, lane, lane))
-            if args.symlinks:
-                os.symlink(args.fastq1, fastq1_dest)
-                os.symlink(args.fastq2, fastq2_dest)
+            if symlinks:
+                os.symlink(fastq1, fastq1_dest)
+                os.symlink(fastq2, fastq2_dest)
             else:
-                shutil.copyfile(args.fastq1, fastq1_dest)
-                shutil.copyfile(args.fastq2, fastq2_dest)
+                shutil.copyfile(fastq1, fastq1_dest)
+                shutil.copyfile(fastq2, fastq2_dest)
             
             sample_number += 1
     # now save everything to charon
     print "updaiting charon-dev"
-    writeProjectData(charon_data, args.token, args.url)
-
-    print "now run this for each FC"
-    print "    python <path/to>/ngi_pipeline/scripts/ngi_pipeline_start.py organize <path/to/FC>"
-    print "and then this to start the analysis"
-    print "    python <path/to>/ngi_pipeline/scripts/ngi_pipeline_start.py analyze project<path/to>y/DATA/{}".format(project_identifier)
+    writeProjectData(charon_data, token, url, facility)
+    print "now run the following:"
+    print "PATH_TO_NGI=<path/to>/ngi_pipeline>"
+    print "PATH_TO_FC={}".format(os.getcwd())
+    print "PATH_TO_DATA=<path/to/data>"
+    for FC in FCs:
+        print "python $PATH_TO_NGI/scripts/ngi_pipeline_start.py organize $PATH_TO_FC/{}".format(FC)
+    print "python $PATH_TO_NGI/scripts/ngi_pipeline_start.py analyze $PATH_TO_DATA/DATA/{}".format(project_identifier)
 
 
 
@@ -110,26 +129,30 @@ def create_sample_name(project_identifier, sample_number, facility):
 
 
 
-def writeProjectData(data, token, base_url):
+def writeProjectData(data, token, base_url, facility):
     project=data
     samples=project.pop('samples', None)
     url=base_url+'/api/v1/project'
     projson=json.dumps(project)
     writeToCharon(projson, url, token)
-    for sid in samples:
+    if facility == 'uppsala':
+        # in uppsala case do not create samples, lib-preps, and runs level, they will be guessed while organising the FC
+        return None
+    for sid in samples.keys():
         libs=samples[sid].pop('libs', None)
         sampjson=json.dumps(samples[sid])
         url=base_url+'/api/v1/sample/'+project['projectid']
         writeToCharon(sampjson, url, token)
-        for lib in libs:
+        for lib in libs.keys():
             seqruns=libs[lib].pop('seqruns', None)
             libjson=json.dumps(libs[lib])
             url=base_url+'/api/v1/libprep/'+project['projectid']+"/"+sid
             writeToCharon(libjson, url, token)
-            for seqrun in seqruns:
+            for seqrun in seqruns.keys():
                 seqjson=json.dumps(seqruns[seqrun])
                 url=base_url+'/api/v1/seqrun/'+project['projectid']+"/"+sid+"/"+lib
                 writeToCharon(seqjson, url, token)
+    return None
 
 
 def writeToCharon(jsonData, url, token):
@@ -145,24 +168,36 @@ def cleanCharon(pid, url, token):
 
 
 
-def create_proj_name(project_name=None, facility='stockholm'):
-    if project_name == None:
-        if facility == 'stockholm':
-            project_name_header = "J.Doe"
-            year = generate_random_numbers(2)
-            pnum = generate_random_numbers(2)
-            project_name = "{}_{}_{}".format(project_name_header, year, pnum)
+def create_proj_name(project_name=None, facility='stockholm', token=None, url=None):
+    project_id = ""
+    while True:
+        if project_name == None:
+            if facility == 'stockholm':
+                project_name_header = "J.Doe"
+                year = generate_random_numbers(2)
+                pnum = generate_random_numbers(2)
+                project_name = "{}_{}_{}".format(project_name_header, year, pnum)
+            else:
+                project_name = "{}-{}".format(generate_random_chars(2), generate_random_numbers(4))
         else:
-            project_name = "{}-{}".format(generate_random_chars(2), generate_random_numbers(4))
-    else:
-        project_name = project_name
+            project_name = project_name
 
-    if facility == 'stockholm':
-        project_id = "P{}".format(generate_random_numbers(3))
-    else:
-        project_id = project_name
-    #TODO: CHECK THAT THE PROJECT DOES NOT EXISTS ALREADY
+        if facility == 'stockholm':
+            project_id = "P{}".format(generate_random_numbers(3))
+        else:
+            project_id = project_name
+        # check if this project already exists in charon
+        if project_id_not_in_charon(project_id, token, url):
+            break
     return (project_name, project_id)
+
+def project_id_not_in_charon(project_id, token, url):
+    # returns True if the project_id is not already present in charon. False otherwise
+    session = requests.Session()
+    headers = {'X-Charon-API-token': token, 'content-type': 'application/json'}
+    # check if this project already exists
+    return project_id not in [project['projectid'] for project in  session.get(url+'/api/v1/projects', headers=headers).json()['projects']]
+
 
 
 def create_fake_FC():
@@ -178,7 +213,7 @@ def create_fake_FC():
         # FC position
         FC_pos = random.choice(["A", "B"])
         # FC name
-        FC_name = generate_random_chars(9)
+        FC_name = generate_random_chars(7) + 'XX'
         # now compose the name
         run_name = "{}_{}_{}_{}{}".format(date, instrument, run_id, FC_pos, FC_name)
         # check if this FC exists already
@@ -203,20 +238,27 @@ def generate_random_instrument():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("""This script generates a test suite for NGI-pipeline and piper testing. Needs as input two fastq files (read 1 and read 2) and creates as many FC as specified. The enviorment variables need to point to cahron-dev """)
-    parser.add_argument('--fastq1', help="path to fastq file containing read 1", type=str,  required=True)
-    parser.add_argument('--fastq2', help="path to fastq file containing read 2", type=str,  required=True)
-    parser.add_argument('--FC', help="number of FC to be created", type=int,  required=True)
-    parser.add_argument('--project-name', help="Name of the project that will be simulated (something like J.Doe_16_01)", type=str,
-        default=None)
+    parser = argparse.ArgumentParser("""This script generates a test suite for NGI-pipeline and piper testing. It needs as input two fastq files (read 1 and read 2) and creates as many FC as specified. The enviorment variables need to point to cahron-dev. It runs in two modalities, create and delete. The former creates the folder structure and the corresponding charon-dev entries, the latter deletes entries in charon-dev.""")
+    # general options
     parser.add_argument('--facility', help="facility sequencing the project (stkholm, uppsala)", type=str, default="stockholm",
-        choices=('stockholm, uppsala'))
+        choices=("stockholm", "uppsala"))
     parser.add_argument("-t", "--token", dest="token", default=os.environ.get('CHARON_API_TOKEN'),
             help="Charon API Token. Will be read from the env variable CHARON_API_TOKEN if not provided")
     parser.add_argument("-u", "--url", dest="url", default=os.environ.get('CHARON_BASE_URL'),
             help="Charon base url. Will be read from the env variable CHARON_BASE_URL if not provided")
-    parser.add_argument("--delete", help="delete from charon the project id specified", type=str )
-    parser.add_argument("--symlinks", help="instead of copying files creates symlinks", action='store_true', default=False)
+    # add subparsers
+    subparsers = parser.add_subparsers(help="subcommands")
+    # delete options
+    parser_delete = subparsers.add_parser('delete', help="Command for delete (i.e., clean) a projet in charon-dev")
+    parser_delete.add_argument('--project-id', help="Project-id (e.g., P1021 or Uppsala project name) that will be deleted", type=str, required=True)
+    # create options
+    parser_create = subparsers.add_parser('create', help="Command for create  a projet in charon-dev")
+    parser_create.add_argument('--fastq1', help="path to fastq file containing read 1", type=str,  required=True)
+    parser_create.add_argument('--fastq2', help="path to fastq file containing read 2", type=str,  required=True)
+    parser_create.add_argument('--FC', help="number of FC to be created", type=int,  required=True)
+    parser_create.add_argument('--project-name', help="Name of the project that will be simulated (something like J.Doe_16_01)", type=str,
+        default=None)
+    parser_create.add_argument("--symlinks", help="instead of copying files creates symlinks", action='store_true', default=False)
     args = parser.parse_args()
 
     if not args.token :
