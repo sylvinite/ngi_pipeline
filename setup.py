0024f538a2c689882dfea0c0c09f5791020fe311
Isak Sylvin <isak.sylvin@scilifelab.se>
2016-06-29 16:01:45 +0200

    Added Contributing.md and moved version number

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import glob
-
+from ngi_pipeline import __version__
 from setuptools import setup, find_packages
+
+version = __version__
 
 try:
     with open("requirements.txt", "r") as f:
@@ -15,7 +17,7 @@ setup(name="ngi_pipeline",
       description = "Infrastructure/analysis pipeline scripts.",
       license = "MIT",
       url="https://github.com/scilifelab/scilifelab_pipeline",
-      version="0.1.0",
+      version=version,
       install_requires=install_requires,
       scripts = glob.glob('scripts/*py'),
       packages=find_packages()
